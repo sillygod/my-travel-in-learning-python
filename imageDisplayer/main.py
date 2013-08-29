2420afa52b6fa1f85ec0539167520aa207b753de
sillygod <sillygod@livemail.tw>
2013-08-29 14:39:21 +0800

    add filebrowser lists the current directory file, enable to click each item and dispaly it

--- imageDisplayer/main.py
+++ imageDisplayer/main.py
@@ -1,3 +1,7 @@
+'''
+	http://pyqt.sourceforge.net/Docs/PyQt4/qgridlayout.html#addWidget-3
+'''
+
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import sys
@@ -15,22 +19,26 @@ class imgDisplay(QGraphicsScene):
 		self.img = QImageReader(fname)
 		self.rect = QRect(0, 0, self.img.size().width(), self.img.size().height())
 		self.setSceneRect(QRectF(self.retRect()))
-		self.imgLst = []
+		self._container = None
 
 		
 		if self.img.imageCount()>1:  #self.img.supportsAnimation wierd why jpg is also support?
 			super().startTimer(self.img.nextImageDelay())
 			self._distractImg()
 		else:
-			self.addPixmap(QPixmap.fromImageReader(self.img))
+			self._container = QPixmap.fromImageReader(self.img)
+			self.addPixmap(self._container)
+
+	def saveImg(self, fileName):
+		self._container.save(fileName)
+
 
 	def retRect(self):
 		return self.rect
 
 	def _distractImg(self):
-		for i in range(self.img.imageCount()):
-			self.imgLst.append( QPixmap.fromImageReader(self.img) )
-		self._iter = iter(self.imgLst)
+		self._container = [ QPixmap.fromImageReader(self.img) for i in range(self.img.imageCount()) ]
+		self._iter = iter(self._container)
 			
 
 	def timerEvent(self, QTimerEvent):
@@ -38,22 +46,49 @@ class imgDisplay(QGraphicsScene):
 		try:
 			self.addPixmap(next(self._iter))
 		except:
-			self._iter = iter(self.imgLst)
+			self._iter = iter(self._container)
 			self.addPixmap(next(self._iter))
 
 		self.update()
 
 
 
-class fileBrowser:
+class fileBrowser(QListView):
 	'''
-		os.listdir(path)
-		os.getcwd()
-		os.path.isdir() has some function
+		
 	'''
-	def __init__(self, path=None):
-		self._path = getcwd() if path == None else path
+	def __init__(self, view=None, path=None):
+		super().__init__()
+		self._path = os.getcwd() if path == None else path
+		self.setContent()
+		self._view = view
 
+	
+		self.connect(self, SIGNAL('clicked(QModelIndex)'), self.loadContent)
+
+
+	def loadContent(self, index):
+		fname = os.path.join(self.path, index.data())
+		if os.path.splitext(fname)[1].lower() in ''.join(self.retSupFmt()):   #check the extension
+			obj = imgDisplay(fname)
+			self._view.setScene(obj)
+		else:
+			print('filter')
+
+	def retSupFmt(self):
+		''' return a generator of supported file format'''
+		return ('*.{}'.format(bytearray(x).decode()) for x in QImageReader.supportedImageFormats())
+
+
+
+	def setContent(self):
+		self.model = QStandardItemModel(self)
+
+		for fname in os.listdir(self.path):
+			name = QStandardItem(fname)
+			self.model.appendRow(name)
+
+		self.setModel(self.model)
 
 
 	
@@ -64,6 +99,7 @@ class fileBrowser:
 	@path.setter
 	def path(self, value):
 		self._path = value
+		self.setContent()
 
 
 
@@ -71,22 +107,34 @@ class mainWindow(QMainWindow):
 	'''
 		dockwidget can only dock in qmainwindow
 
+		mission: add file browser next, and add icon control button as best as I can
+
 	'''
 
 	def __init__(self):
 		super().__init__()
 		self.act = {}
+		self.ctWidget = QWidget()
 		self.view = QGraphicsView()
-		menu = self.menuBar()
+		self.fileBrowser = fileBrowser(self.view)
+		
 		
 		self.createAction('Open', self.openFile)
-
+		self.createAction('Save', self.saveFile)
 		#create menu
+		menu = self.menuBar()
 		fileMenu = menu.addMenu('&File')
 		fileMenu.addAction(self.act['Open'])
+		fileMenu.addAction(self.act['Save'])
 
 		
-		self.setCentralWidget(self.view)
+		#create a widget contain filebrowser and view
+		#set the layout
+		grid = QGridLayout()
+		grid.addWidget(self.fileBrowser, 0, 0, 1, -1) #-1?
+		grid.addWidget(self.view, 0, 1, 1, 5)
+		self.ctWidget.setLayout(grid)
+		self.setCentralWidget(self.ctWidget)
 		self.view.show()
 
 
@@ -97,20 +145,29 @@ class mainWindow(QMainWindow):
 		'''
 		act = QAction(name, self)
 		act.triggered.connect(slot)
-		self.act['Open'] = act
+		self.act[name] = act
+
+	def saveFile(self):
+		supFmt = ('*.{}'.format(bytearray(x).decode()) for x in QImageReader.supportedImageFormats())
+		filter = 'Images ({})'.format(' '.join(supFmt))
+		fname = QFileDialog.getSaveFileName(self, 'save', '.', filter)
+		if fname:
+			self.view.scene().saveImg(fname)
 
 
 
 	def openFile(self):
 
-		supFmt = ('*.{}'.format(bytearray(x).decode()) for x in QImageReader.supportedImageFormats())
+		supFmt = self.fileBrowser.retSupFmt()
 		dir = '.'
 		filter = 'Images ({})'.format(' '.join(supFmt))
 		fname = QFileDialog.getOpenFileName(self, 'open', dir, filter)
-
-		obj = imgDisplay(fname)
-		self.view.setScene(obj)
-		self.resize(obj.retRect().width(), obj.retRect().height()+50)
+		if fname:
+			obj = imgDisplay(fname)
+			self.view.setScene(obj)
+			self.resize(obj.retRect().width(), obj.retRect().height()+50)
+			self.fileBrowser.path = os.path.dirname(fname)
+			# need to rethink how to resize ....
 
 
 
@@ -122,5 +179,4 @@ if __name__ == '__main__':
 
 	obj = mainWindow()
 	obj.show()
-
 	sys.exit(app.exec_())
