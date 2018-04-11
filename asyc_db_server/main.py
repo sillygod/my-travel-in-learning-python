16069a6ac0895815f8c158a98980706eaab7ff2a
sillygod <sillygod@livemail.tw>
2018-04-11 12:25:10 +0800

    complete first stage of asyc_db

--- asyc_db_server/main.py
+++ asyc_db_server/main.py
@@ -1,4 +1,4 @@
-"""
+"""This is a practice about simulating redis db
 """
 from collections import namedtuple
 from io import BytesIO
@@ -23,13 +23,96 @@ class ProtocolHandler:
     """unpack client requests and serialize server response
     """
 
+    def __init__(self):
+        """according to the redis protocol, implement the corresponding handler
+        """
+        self.handlers = {
+            b'+': self.handle_simple_string,
+            b'-': self.handle_error,
+            b':': self.handle_integer,
+            b'$': self.handle_string,
+            b'*': self.handle_array,
+            b'%': self.handle_dict,
+        }
+
     def handle_request(self, socket_file):
         # parse a request from the client info it's component parts.
-        pass
+        # equals to unserialize
+        first_byte = socket_file.read(1)
+        if not first_byte:
+            raise Disconnect()
+
+        try:
+            return self.handlers[first_byte](socket_file)
+        except KeyError:
+            raise CommandError('bad request')
+        
+    def handle_simple_string(self, socket_file):
+        return socket_file.readline().rstrip(b'\r\n')
+
+    def handle_error(self, socket_file):
+        return Error(socket_file.readline().rstrip(b'\r\n'))
+
+    def handle_integer(self, socket_file):
+        return int(socket_file.readline().rstrip(b'\r\n'))
+
+    def handle_string(self, socket_file):
+        length = self._parseNum(socket_file)
+        if length == -1:
+            return None
+        
+        # can not use readline.. the content may contain \r\n
+        length += 2 # include the \r\n
+        return socket_file.read(length)[:-2]
+
+    def handle_array(self, socket_file):
+        num = self._parseNum(socket_file)
+        return [self.handle_request(socket_file) for _ in range(num)]
+
+    def handle_dict(self, socket_file):
+        num_dicts = self._parseNum(socket_file)
+        elements = [self.handle_request(socket_file) for _ in range(num_dicts*2)]
+        keys = elements[::2]
+        values = elements[1::2]
+        return dict(zip(keys, values))
+
+
+    def _parseNum(self, socket_file):
+        return int(socket_file.readline().rstrip(b'\r\n'))
 
     def write_response(self, socket_file, data):
         # serialize the response data and send it to the client.
-        pass
+        buf = BytesIO() 
+        buf.write(self._serialize(data).encode('utf-8'))
+        socket_file.write(buf.getvalue())
+        socket_file.flush()
+        buf.close()
+
+
+    def _serialize(self, data) -> str:
+        content = ''
+        
+        if isinstance(data, str):
+            content = f"${len(data)}\r\n{data}\r\n"
+        elif isinstance(data, bytes):
+            content = f"${len(data)}\r\n{data.decode('utf-8')}\r\n"
+        elif isinstance(data, int):
+            content = f":{data}\r\n"
+        elif isinstance(data, Error):
+            content = f"-{data.message}\r\n"
+        elif isinstance(data, (list, tuple)):
+            content = f"*{len(data)}\r\n"
+            return content + "".join([self._serialize(item) for item in data])
+        elif isinstance(data, dict):
+            content = f"%{len(data)}\r\n"
+            return content + "".join([self._serialize(key)+self._serialize(value) for key, value in data.items()])
+        elif data is None:
+            content = "$-1\r\n"
+        else:
+            # cheers for python3.6 new feature: f string (performance also good)
+            raise CommandError(f'unrecognized type: {type(data)}')
+
+        return content
 
 class Server:
 
@@ -43,6 +126,8 @@ class Server:
         self._kv = {}
 
     def connection_handler(self, conn, address):
+        # process the args from function do_read() for more detail
+        # you can see the source in StreamServer of gevent.
         # Convert "conn" (a socket object) into a file-like object
         socket_file = conn.makefile('rwb')
 
@@ -59,13 +144,100 @@ class Server:
 
             self._protocol.write_response(socket_file, resp)
 
+    def get_command(self, name:str):
+        if hasattr(self, name.lower()):
+            func = getattr(self, name.lower())
+            return func
+        return None
+
     def get_response(self, data):
         # Here we'll actually unpack the data sent by the client, execute the 
         # command they specified, and pass back the return value.
-        pass
+        if not isinstance(data, list):
+            try:
+                data = data.split()
+            except:
+                raise CommandError('request must be list or string.')
+
+        if not data:
+            raise CommandError('missing command')
+
+        command = data[0].lower().decode('utf-8')
+        func = self.get_command(command)
+        if func is None:
+            raise CommandError(f'unrecognize command: {command}')
+
+        return func(*data[1:])
     
+    def get(self, key):
+        return self._kv.get(key)
+
+    def set(self, key, value):
+        self._kv[key] = value
+        return 1
+
+    def delete(self, key):
+        if key in self._kv:
+            del self._kv[key]
+            return 1
+
+        return 0
+
+    def flush(self):
+        kvlen = len(self._kv)
+        self._kv.clear()
+        return kvlen
+
+    def mget(self, *keys):
+        return [self._kv.get(key) for key in keys]
+
+    def mset(self, *item):
+        data = zip(item[::2], item[1::2])
+        for k, v in data:
+            self._kv[k] = v
+        return len(item) // 2
+
     def run(self):
         self._server.serve_forever()
 
+
+class Client:
+
+    def __init__(self, host='127.0.0.1', port='31337'):
+        self._protocol = ProtocolHandler()
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect((host, port))
+        self._fn = self._socket.makefile('rwb')
+
+    def execute(self, *args):
+        self._protocol.write_response(self._fn, args)
+        resp = self._protocol.handle_request(self._fn)
+        if isinstance(resp, Error):
+            raise CommandError(resp.message)
+        return resp
+
+    def get(self, key):
+        return self.execute('GET', key)
+    
+    def set(self, key, value):
+        return self.execute('SET', key, value)
+
+    def delete(self, key):
+        return self.execute('DELETE', key)
+
+    def flush(self):
+        return self.execute('FLUSH')
+
+    def mget(self, *keys):
+        return self.execute('MGET', *keys)
+
+    def mset(self, *items):
+        return self.execute('MSET', *items)
             
 
+if __name__ == '__main__':
+    from gevent import monkey
+    monkey.patch_all()
+
+    s = Server()
+    s.run()
--- asyc_db_server/readme.md
+++ asyc_db_server/readme.md
@@ -3,7 +3,20 @@
 
 `wire protocol` refers to the way of getting data from point to point
 
-然後這篇作者講到他在如何傳送資料上，遇到點問題，不知道要如何做serialization @@? 我想大概是他不知道要怎麼去猜測redis client server之間的傳送，幸好redis 官網有提供[protocol 文檔](https://redis.io/topics/protocol)，需要注意的是文檔講到這個protocol，是只適用於client-server，若是redis cluster的話是用另外一種
+然後這篇作者講到他在如何傳送資料上，遇到點問題，不知道要如何做serialization @@? 我想大概是他不知道要怎麼去猜測redis client server之間的傳送，幸好redis 官網有提供[protocol 文檔](https://redis.io/topics/protocol)，需要注意的是文檔講到這個protocol，是只適用於client-server，若是redis cluster的話是用另外一種。
+
+作者提出可以extend這個project
+
+>To extend the project, you might consider:
+
+>Add more commands!
+>Use the protocol handler to implement an append-only command log
+>More robust error handling
+>Allow client to close connection and re-connect
+>Logging
+>Re-write to use the standard library's SocketServer and ThreadingMixin
+
+有時間會來try try
 
 # References
 
