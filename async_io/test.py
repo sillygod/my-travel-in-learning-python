f71e36dfa376e1fe4b3b9e1bbe7f5fe138a74ebd
sillygod <sillygod@livemail.tw>
2020-07-24 21:15:49 +0800

    revise async_io

--- async_io/test.py
+++ async_io/test.py
@@ -6,15 +6,25 @@ from urllib.request import urlopen
 mtart = time.time()
 gevent.monkey.patch_all()
 
+import asyncio
 
 
-urls = ['http://www.google.com', 'http://www.yandex.ru', 'http://www.python.org', 'http://www.python.org', 'http://www.python.org'] * 3
+@asyncio.coroutine
+def hi():
+    print('HI')
+
+
+urls = [
+    'http://www.google.com', 'http://www.yandex.ru', 'http://www.python.org',
+    'http://www.python.org', 'http://www.python.org'
+] * 3
 
 
 def print_head(url):
     r = requests.get(url)
     data = r.text
     print('{}: {} bytes'.format(url, len(data)))
+
 
 jobs = [gevent.spawn(print_head, _url) for _url in urls]
 
